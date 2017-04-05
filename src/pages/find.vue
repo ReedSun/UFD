85a0dbb648e5ba09854c1a461a4693f43a3c0702
ReedSun <sunhongzhao@foxmail.com>
2017-04-05 21:45:55 +0800

    发现页面接口测试成功

--- src/pages/find.vue
+++ src/pages/find.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="find">
-    <p>这里是find页面</p>
+    <p>
+      <button @click="show='top250'">Top250</button>
+      <button @click="show='box-office'">北美票房榜</button>
+    </p>
+    <find-top250 v-if="show==='top250'"/>
+    <find-box-office v-if="show==='box-office'"/>
   </div>
 </template>
 <script>
+  import findTop250 from '@/components/find/find-top250.vue'
+  import findBoxOffice from '@/components/find/find-box-office.vue'
   export default {
-    name: 'find'
+    name: 'find',
+    data: function () {
+      return {
+        show: 'top250'
+      }
+    },
+    components: {
+      'find-top250': findTop250,
+      'find-box-office': findBoxOffice
+    }
   }
 </script>

ade9ff4f4081e51967f06175391166fcbd5ef3d2
ReedSun <superreedsun@gmail.com>
2017-04-07 21:38:46 +0800

    编写hot-show页面

--- src/pages/hot-show.vue
+++ src/pages/hot-show.vue
@@ -1,26 +1,50 @@
 <template>
   <div class="hot-show">
-    <p>
-      <button @click="show='now'">正在热映</button>
-      <button @click="show='future'">即将上映</button>
-    </p>
-    <being-relesed v-if="show==='now'"/>
-    <future-relesed v-if="show==='future'"/>
+    <mt-navbar class="page-part" v-model="selected">
+      <mt-tab-item id="1">正在热映</mt-tab-item>
+      <mt-tab-item id="2">即将上映</mt-tab-item>
+    </mt-navbar>
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="1">
+        <mt-cell v-for="item in movieData.in_theaters" :title="item.title"></mt-cell>
+      </mt-tab-container-item>
+      <mt-tab-container-item id="2">
+        <mt-cell v-for="item in movieData.coming_soon" :title="item.title"></mt-cell>
+      </mt-tab-container-item>
+    </mt-tab-container>
   </div>
 </template>
 <script>
-  import beingRelesed from '@/components/hot-show/being-relesed'
-  import futureRelesed from '@/components/hot-show/future-relesed'
+  import axios from 'axios'
   export default {
     name: 'hotShow',
     data: function () {
       return {
-        show: 'now'
+        show: 'now',
+        selected: '1',
+        movieData: {
+          in_theaters: [],
+          coming_soon: []
+        }
       }
     },
-    components: {
-      'being-relesed': beingRelesed,
-      'future-relesed': futureRelesed
+    created: function () {
+      this.getMoive('/api/movie/in_theaters', 'in_theaters')
+    },
+    mounted: function () {
+      this.getMoive('/api/movie/coming_soon', 'coming_soon')
+    },
+    methods: {
+      getMoive: function (url, dataLocation) {
+        axios({
+          method: 'GET',
+          url: url
+        }).then((response) => {
+          this.movieData[dataLocation] = response.data.subjects
+        }).catch((error) => {
+          console.log(error)
+        })
+      }
     }
   }
 </script>

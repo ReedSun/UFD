af7d7d9e65e42b109e6205c5cca37cc2fc84eff0
ReedSun <superreedsun@gmail.com>
2017-04-21 23:48:26 +0800

    improve  movie-detail页面拆分出movie-detial-message组件

---
+++ src/components/movie-detail-message.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="movie-detail-message">
+    <div class="movie-intr-wrapper">
+      <div class="movie-intr">
+        <h1>{{title}}</h1>
+        <p>上映年份：{{year}}</p>
+        <p>影片类型: 
+          <span class="detail-item-item" v-for="item in genres">{{item}}</span>
+        </p>
+        <p>原名：{{originalTitle}}</p>
+        <p>又名：
+          <span class="detail-item-item" v-for="item in aka">{{item}}</span>
+        </p>
+        <p>制片国家/地区：
+          <span class="detail-item-item" v-for="item in countries">{{item}}</span>
+        </p>
+      </div>
+      <div class="movie-rate">
+        <h1>{{rate}}</h1>
+        <p>{{ratePeople}}人</p>
+      </div>    
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'movie-detail-message',
+    props: {
+      title: {
+        type: String,
+        require: true
+      },
+      year: {
+        type: String,
+        require: true
+      },
+      originalTitle: {
+        type: String,
+        require: true
+      },
+      genres: {
+        type: Array,
+        require: true
+      },
+      aka: {
+        type: Array,
+        require: true
+      },
+      countries: {
+        type: Array,
+        require: true
+      },
+      rate: {
+        type: String,
+        require: true
+      },
+      ratePeople: {
+        type: String,
+        require: true
+      }
+    }
+  }
+</script>
+<style>
+  .movie-intr-wrapper {
+    position: relative;
+    padding: 20px;
+    border-bottom: 1px solid #eee;
+  }
+  .movie-intr {
+    text-align: left;
+    padding-right: 90px;
+  }
+  .movie-intr>h1 {
+    font-size: 30px;
+    margin-bottom: 5px;
+  }
+  .movie-intr>p {
+    color: #969696;
+    font-size: 12px;
+    line-height: 1.5em;
+  }
+  .detail-item-item:after {
+    content: '/';
+    padding-left: 4px;
+    padding-right: 4px;
+  }
+  .detail-item-item:last-child:after {
+    content: '';
+  }
+  .movie-rate {
+    position: absolute;
+    right: 20px;
+    top: 50%;
+    transform: translateY(-50%);
+    padding: 10px;
+    box-shadow: 4px 4px 10px 4px #ccc;
+  }
+  .movie-rate>h1 {
+    font-size: 30px;
+    color: #232323;
+  }
+  .movie-rate>p {
+    color: #969696;
+    font-size: 12px;
+    line-height: 1.5em;    
+  }
+</style>
--- src/pages/movie-detail.vue
+++ src/pages/movie-detail.vue
@@ -1,26 +1,7 @@
 <template>
   <div class="moive-detail">
     <movie-detail-header :img="data.images.large"></movie-detail-header>
-    <div class="movie-intr-wrapper">
-      <div class="movie-intr">
-        <h1>{{data.title}}</h1>
-        <p>上映年份：{{data.year}}</p>
-        <p>影片类型: 
-          <span class="detail-item-item" v-for="item in data.genres">{{item}}</span>
-        </p>
-        <p>原名：{{data.original_title}}</p>
-        <p>又名：
-          <span class="detail-item-item" v-for="item in data.aka">{{item}}</span>
-        </p>
-        <p>制片国家/地区：
-          <span class="detail-item-item" v-for="item in data.countries">{{item}}</span>
-        </p>
-      </div>
-      <div class="movie-rate">
-        <h1>{{data.rating.average}}</h1>
-        <p>{{data.ratings_count}}人</p>
-      </div>    
-    </div>
+    <movie-detail-message :title="data.title" :year="data.year" :originalTitle="data.original_title" :genres="data.genres" :aka="data.aka" :countries="data.countries" :rate="data.rating.average" :ratePeople="data.ratings_count"></movie-detail-message>
     <div class="movie-summary">
       <p class="movie-summary-title">简介</p>
       <p class="movie-summary-main" v-for="item in movieSummaryArr">{{item}}</p>
@@ -30,10 +11,12 @@
 <script>
   import axios from 'axios'
   import movieDetailHeader from '@/components/movie-detail-header'
+  import movieDetailMessage from '@/components/movie-detail-message'
   export default {
     name: 'movie-detail',
     components: {
-      movieDetailHeader
+      movieDetailHeader,
+      movieDetailMessage
     },
     data: function () {
       return {
@@ -61,48 +44,6 @@
   }
 </script>
 <style>
-  .movie-intr-wrapper {
-    position: relative;
-    padding: 20px;
-    border-bottom: 1px solid #eee;
-  }
-  .movie-intr {
-    text-align: left;
-  }
-  .movie-intr>h1 {
-    font-size: 30px;
-    margin-bottom: 5px;
-  }
-  .movie-intr>p {
-    color: #969696;
-    font-size: 12px;
-    line-height: 1.5em;
-  }
-  .detail-item-item:after {
-    content: '/';
-    padding-left: 4px;
-    padding-right: 4px;
-  }
-  .detail-item-item:last-child:after {
-    content: '';
-  }
-  .movie-rate {
-    position: absolute;
-    right: 20px;
-    top: 50%;
-    transform: translateY(-50%);
-    padding: 10px;
-    box-shadow: 4px 4px 10px 4px #ccc;
-  }
-  .movie-rate>h1 {
-    font-size: 30px;
-    color: #232323;
-  }
-  .movie-rate>p {
-    color: #969696;
-    font-size: 12px;
-    line-height: 1.5em;    
-  }
   .movie-summary {
     padding: 20px;
     text-align: left;

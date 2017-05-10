407d5dfc75bd2dc7c1acf5c64b69d4a117902b90
ReedSun <superreedsun@gmail.com>
2017-05-10 21:01:04 +0800

    fix: close load indicator when toggle tab; close #4

--- src/main.js
+++ src/main.js
@@ -2,15 +2,21 @@
  * @Author: ReedSun
  * @Date: 2017-04-04 09:48:37
  * @Last Modified by: ReedSun
- * @Last Modified time: 2017-04-25 22:24:47
+ * @Last Modified time: 2017-05-10 20:59:15
  */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Indicator } from 'mint-ui'
 
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  Indicator.close()
+  next()
+})
 
 /* eslint-disable no-new */
 new Vue({

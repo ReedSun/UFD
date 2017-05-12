789b5967331a33fc7ba4d2a15119f5fbabf8e8f7
ReedSun <superreedsun@gmail.com>
2017-05-12 21:17:29 +0800

    add: movie-people.vue => header component

--- src/pages/movie-people.vue
+++ src/pages/movie-people.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <p>这里是影人页面</p>
-    <p>{{$route.params.id}}</p>
+  <div class="movie-people">
+    <mt-header class="header" title="电影" fixed>
+      <div @click="$router.go(-1)" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </div>
+    </mt-header>
   </div>
 </template>
+
+<script>
+  import { Header, Button } from 'mint-ui'
+  export default {
+    components: {
+      mtHeader: Header,
+      mtButton: Button
+    }
+  }
+</script>

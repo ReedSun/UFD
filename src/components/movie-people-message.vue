e435349512fb0e63e9c7a1ae567b421307e1f62e
ReedSun <superreedsun@gmail.com>
2017-05-16 20:13:17 +0800

    close #8 : hide info when it is null

--- src/components/movie-people-message.vue
+++ src/components/movie-people-message.vue
@@ -3,13 +3,13 @@
     <div class="people-message-wrapper">
       <div class="people-message">
         <h1>{{name}}</h1>
-        <p>性别：{{gender}}</p>
-        <p>英文名：{{name_en}}</p>
-        <p>出生地：{{born_place}}</p>
-        <p>更多中文名: 
+        <p v-if="gender">性别：{{gender}}</p>
+        <p v-if="name_en">英文名：{{name_en}}</p>
+        <p v-if="born_place">出生地：{{born_place}}</p>
+        <p v-if="aka.length">更多中文名:
           <span class="detail-item-item" v-for="item in aka">{{item}}</span>
         </p>
-        <p>更多英文名: 
+        <p v-if="aka_en.length">更多英文名: 
           <span class="detail-item-item" v-for="item in aka_en">{{item}}</span>
         </p>
       </div>
@@ -39,12 +39,16 @@
       aka: {
         type: Array,
         require: true,
-        dafault: []
+        default: () => {
+          return []
+        }
       },
       aka_en: {
         type: Array,
         require: true,
-        dafault: []
+        default: () => {
+          return []
+        }
       }
     }
   }
